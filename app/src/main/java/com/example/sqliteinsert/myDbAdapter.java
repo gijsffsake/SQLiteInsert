c7d2a893186e91e57abc5071cccd931728d7c16a
gijsffsake <gijsffsake@users.noreply.github.com>
2017-10-24 12:59:10 +0200

    New activity layout added

--- app/src/main/java/com/example/sqliteinsert/myDbAdapter.java
+++ app/src/main/java/com/example/sqliteinsert/myDbAdapter.java
@@ -5,63 +5,40 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class myDbAdapter  {
 
-    myDbHelper helper;
+static class myDbHelper extends SQLiteOpenHelper
+{
+    private static final String CREATE_TABLE = "CREATE TABLE "+ com.example.sqliteinsert.data.UserContract.UserEntity.TABLE_NAME +
+            "( "+ com.example.sqliteinsert.data.UserContract.UserEntity.UID +" INTEGER PRIMARY KEY AUTOINCREMENT ," + com.example.sqliteinsert.data.UserContract.UserEntity.USER_NAME + " VARCHAR(225), " + com.example.sqliteinsert.data.UserContract.UserEntity.USER_PWD+" VARCHAR(225));";
+    private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+ com.example.sqliteinsert.data.UserContract.UserEntity.TABLE_NAME;
+    private Context context;
 
-    public myDbAdapter(Context context)
-    {
-        helper = new myDbHelper(context);
+    public myDbHelper(Context context) {
+        super(context, com.example.sqliteinsert.data.UserContract.DATABASE_NAME, null, com.example.sqliteinsert.data.UserContract.DATABASE_VERSION);
+        this.context=context;
+        Message.message(context,"Started...");
     }
 
-    public long insertData(String name, String password)
-    {
-        SQLiteDatabase db = helper.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(myDbHelper.NAME, name);
-        contentValues.put(myDbHelper.PASSWORD, password);
-        long id = db.insert(myDbHelper.TABLE_NAME, null , contentValues);
-        return id;
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+
+            db.execSQL(CREATE_TABLE);
+            Message.message(context,"TABLE CREATED");
+        } catch (Exception e) {
+            Message.message(context,""+e);
+        }
     }
-   static class myDbHelper extends SQLiteOpenHelper
-   {
-       private static final String DATABASE_NAME = "myDatabase";
-       private static final String TABLE_NAME = "myTable";
-       private static final int DATABASE_Version = 1;
-       private static final String UID="_id";
-       private static final String NAME = "Name";
-       private static final String PASSWORD= "Password";
-       private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
-               "( " + UID + " INTEGER PRIMARY KEY AUTOINCREMENT ," + NAME + " VARCHAR(225)," + PASSWORD +" VARCHAR(225));";
-       private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+ TABLE_NAME;
-       private Context context;
 
-       public myDbHelper(Context context) {
-           super(context, DATABASE_NAME, null, DATABASE_Version);
-           this.context=context;
-           Message.message(context,"Started...");
-       }
-
-       @Override
-       public void onCreate(SQLiteDatabase db) {
-           try {
-
-               db.execSQL(CREATE_TABLE);
-               Message.message(context,"TABLE CREATED");
-           } catch (Exception e) {
-              Message.message(context,""+e);
-           }
-       }
-
-       @Override
-       public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-           try {
-               Message.message(context,"OnUpgrade");
-               db.execSQL(DROP_TABLE);
-               onCreate(db);
-           }catch (Exception e) {
-               Message.message(context,""+e);
-                          }
-       }
-   }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            Message.message(context,"OnUpgrade");
+            db.execSQL(DROP_TABLE);
+            onCreate(db);
+        }catch (Exception e) {
+            Message.message(context,""+e);
+        }
+    }
 }
+
